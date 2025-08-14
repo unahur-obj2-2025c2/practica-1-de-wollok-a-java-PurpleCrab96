66d2085baff6cb3f499fb769f49b023e74c58912
PurpleCrab96 <gastonexequielfernandez@gmail.com>
2025-08-13 21:44:37 -0300

    planPremium

---
+++ src/main/java/ar/edu/planes/PlanPremium.java
@@ -0,0 +1,15 @@
+package ar.edu.planes;
+
+public class PlanPremium implements PlanSuscribible{
+    private double costoPlan;
+
+    private PlanPremium(double costoPlan) {
+        this.costoPlan = costoPlan;
+    }
+
+    @Override
+    public double costoPlan(Usuario usuario) {
+        return costoPlan;
+    }
+
+}
